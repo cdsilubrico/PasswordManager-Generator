3e1f22870d9eb701de1f0dad783e89148bac9f58
clowiesilubrico <clowiesilubrico@gmail.com>
2021-03-30 13:21:28 +0800

    Add Global Controller

---
+++ src/Controller/GlobalController.java
@@ -0,0 +1,28 @@
+package Controller;
+
+import javax.swing.*;
+
+public class GlobalController {
+
+    private static final String UITheme = "com.sun.java.swing.plaf.windows.WindowsLookAndFeel";
+
+    public static final void UIChange()
+    {
+        try{
+            UIManager.setLookAndFeel(UITheme);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        finally {
+            //
+        }
+    }
+
+
+}
