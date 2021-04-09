dc69eff76590ac07e224b6043575b6582b7401a0
clowiesilubrico <clowiesilubrico@gmail.com>
2021-04-09 17:14:31 +0800

    ADd Account Controller

--- src/Controller/AddAccountController.java
+++ src/Controller/AddAccountController.java
@@ -2,21 +2,37 @@ package Controller;
 
 import javax.swing.*;
 
-import static Controller.GlobalController.UIChange;
+import java.util.ResourceBundle;
+
+import static Controller.GlobalController.*;
 
 public class AddAccountController {
 
+    //ResourceBundle resrouceBundle;
 
     private JPanel Contents;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JButton generateButton;
-    private JComboBox comboBox1;
-    private JComboBox comboBox2;
+    private JTextField loginTextField;
+    private JTextField passwordTextField;
+    private JButton generateResourcesViewsPropertiesButton;
+    private JComboBox AccountNameComboBox;
+    private JButton addResourcesViewsPropertiesButton;
+    private JLabel loginIDLabel;
+    private JLabel passwordLabel;
+    private JComboBox CategoryComboBox;
 
-    AddAccountController()
+//    public AddAccountController() {
+//        resrouceBundle = ResourceBundle.getBundle(getResources());
+//        resrouceBundle.getKeys();
+//    }
+
+    void Launch()
     {
-
+        UIChange();
+        JFrame addAnAccount = new JFrame("Add an Account");
+        addAnAccount.setContentPane((new AddAccountController().Contents));
+        //addAnAccount.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        addAnAccount.pack();
+        addAnAccount.setVisible(true);
     }
 
 
@@ -24,10 +40,14 @@ public class AddAccountController {
     {
         UIChange();
 
-        JFrame mainFrame = new JFrame("Main");
-        mainFrame.setContentPane((new AddAccountController().Contents));
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.pack();
-        mainFrame.setVisible(true);
+        JFrame addAnAccount = new JFrame("Add an Account");
+        addAnAccount.setContentPane((new AddAccountController().Contents));
+        addAnAccount.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        addAnAccount.pack();
+        addAnAccount.setVisible(true);
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
     }
 }
