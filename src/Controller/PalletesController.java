de44d9de2802eff49c1169d30bb98b73630d1937
clowiesilubrico <clowiesilubrico@gmail.com>
2021-04-19 16:36:56 +0800

    Add PalletesController

---
+++ src/Controller/PalletesController.java
@@ -0,0 +1,20 @@
+package Controller;
+
+public class PalletesController {
+
+
+    private final String font = "Helvetica";
+    private final int size = 12;
+
+    private final int preferredWidthBtn = 0;
+    private final int preferredHeightBtn= 0;
+
+
+    public String getFont() {
+        return font;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
