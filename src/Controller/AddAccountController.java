8d32a0491f0d29f1ec4fbee86cf73351431de37b
clowiesilubrico <clowiesilubrico@gmail.com>
2021-04-19 16:35:42 +0800

    Add AddAccountController

--- src/Controller/AddAccountController.java
+++ src/Controller/AddAccountController.java
@@ -1,36 +1,53 @@
 package Controller;
 
+import javax.annotation.Resource;
 import javax.swing.*;
 
+import java.awt.*;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 import static Controller.GlobalController.*;
 
 public class AddAccountController {
 
-    //ResourceBundle resrouceBundle;
+    AddAccountController()
+    {
+        ResourceBundle res = ResourceBundle.getBundle("Views");
+        PalletesController pc = new PalletesController();
+
+        loginTextField.setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
+        passwordTextField.setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
+        loginIDLabel.setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
+        passwordLabel.setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
+        categoryLabel.setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
+        accountNameLabel.setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
+        generateButton.setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
+        addButton.setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
+        CategoryComboBox.setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
+        AccountNameComboBox.setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
+
+    }
+
 
     private JPanel Contents;
     private JTextField loginTextField;
     private JTextField passwordTextField;
-    private JButton generateResourcesViewsPropertiesButton;
+    private JButton generateButton;
     private JComboBox AccountNameComboBox;
-    private JButton addResourcesViewsPropertiesButton;
+    private JButton addButton;
     private JLabel loginIDLabel;
     private JLabel passwordLabel;
     private JComboBox CategoryComboBox;
-
-//    public AddAccountController() {
-//        resrouceBundle = ResourceBundle.getBundle(getResources());
-//        resrouceBundle.getKeys();
-//    }
+    private JLabel categoryLabel;
+    private JLabel accountNameLabel;
 
     void Launch()
     {
         UIChange();
         JFrame addAnAccount = new JFrame("Add an Account");
         addAnAccount.setContentPane((new AddAccountController().Contents));
-        //addAnAccount.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        addAnAccount.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         addAnAccount.pack();
         addAnAccount.setVisible(true);
     }
@@ -45,9 +62,5 @@ public class AddAccountController {
         addAnAccount.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         addAnAccount.pack();
         addAnAccount.setVisible(true);
-    }
-
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
     }
 }
