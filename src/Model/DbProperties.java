c6daccbca59d6f4db9919a43fda2b97f3f7d1721
clowiesilubrico <clowiesilubrico@gmail.com>
2021-05-27 23:51:26 +0800

    Add DbProperties

---
+++ src/Model/DbProperties.java
@@ -0,0 +1,12 @@
+package Model;
+
+public class DbProperties {
+
+    final String JDBC_DRIVER = "";
+    final String JDBC_URL = "";
+
+    final String JDBC_USER = "";
+    final String JDBC_PASS = "";
+
+
+}
