c2b223458b47135019050d8e14b430e95097c967
clowiesilubrico <clowiesilubrico@gmail.com>
2021-06-11 23:56:57 +0800

    Update MainForm

--- src/View/MainForm.java
+++ src/View/MainForm.java
@@ -1,6 +1,9 @@
 package View;
 
+import Controller.Main;
+import Controller.MainController;
 import Controller.PalletesController;
+import Model.Db.DbConnection;
 
 
 import javax.swing.*;
@@ -8,11 +11,17 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import static Controller.GlobalController.UIChange;
 
 
 public class MainForm {
+
+    MainController mc;
 
     private JPanel Contents;
 
@@ -29,7 +38,9 @@ public class MainForm {
     private JLabel accountNameLabel;
     private JLabel categoryLabel;
 
-    public MainForm() {
+    public MainForm(MainController mc) {
+        this.mc = mc;
+
 
         PalletesController pc = new PalletesController();
         accountNameLabel.setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
@@ -40,9 +51,11 @@ public class MainForm {
         btnLoginIdCopy.setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
         btnPasswordCopy.setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
 
-        comboCategory.setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
-        comboSubCategory.setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
-        comboLoginID.setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
+        getComboCategory().setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
+        getComboSubCategory().setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
+        getComboLoginID().setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
+
+
 
         //comboCategory.addItem("ads");
 
@@ -75,46 +88,54 @@ public class MainForm {
 
 
 
-        /*
-        PreparedStatement ps;
-        ResultSet rs;
-
-        try
-        {
-
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/pwmgdb","root","root");
-            ps = con.prepareStatement("select * from category");
-
-            rs = ps.executeQuery();
-
-            while(rs.next())
-            {
-                    comboCategory.addItem(rs.getString("title"));
-            }
-
-            con.close();
 
 
-        }
-        catch(Exception er)
-        {
-            er.printStackTrace();
-        }
-        finally {
-
-        }
-
-         */
 
 
-        comboCategory.addActionListener(new ActionListener() {
+
+
+        getComboCategory().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                
+
             }
         });
+    }
+
+    public MainForm() {
+        MainController mc = new MainController();
+        System.out.println(mc.getCategory().size());
+
+
+
+
+
+//        DbConnection dbc = new DbConnection();
+//        PreparedStatement ps;
+//        ResultSet rs;
+//
+//        try
+//        {
+//
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//            //Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/pwmgdb","root","root");
+//            ps = dbc.dbConnection().prepareStatement("select * from category");
+//            rs = ps.executeQuery();
+//
+//            while(rs.next())
+//            {
+//                comboCategory.addItem(rs.getString("title"));
+//            }
+//
+//
+//        }
+//        catch(Exception er)
+//        {
+//            er.printStackTrace();
+//        }
+//        finally {
+//
+//        }
     }
 
     public static void main(String[] args) {
@@ -126,5 +147,37 @@ public class MainForm {
         mainFrame.pack();
         mainFrame.setVisible(true);
 
+    }
+
+    public JTextField getTxtPassword() {
+        return txtPassword;
+    }
+
+    public void setTxtPassword(JTextField txtPassword) {
+        this.txtPassword = txtPassword;
+    }
+
+    public JComboBox getComboCategory() {
+        return comboCategory;
+    }
+
+    public void setComboCategory(JComboBox comboCategory) {
+        this.comboCategory = comboCategory;
+    }
+
+    public JComboBox getComboSubCategory() {
+        return comboSubCategory;
+    }
+
+    public void setComboSubCategory(JComboBox comboSubCategory) {
+        this.comboSubCategory = comboSubCategory;
+    }
+
+    public JComboBox getComboLoginID() {
+        return comboLoginID;
+    }
+
+    public void setComboLoginID(JComboBox comboLoginID) {
+        this.comboLoginID = comboLoginID;
     }
 }
