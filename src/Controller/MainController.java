04a6f5148e85fd3f2b26501bc657eb49b0d18144
clowiesilubrico <clowiesilubrico@gmail.com>
2021-06-13 19:04:42 +0800

    Update MainController

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -1,64 +1,54 @@
 package Controller;
 
-import javax.swing.*;
+import Model.Account;
+import Model.Db.DbConnection;
+import Model.Db.DbManipulate;
+import View.MainForm;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-import static Controller.GlobalController.UIChange;
+public class MainController implements Main {
 
+    private MainForm mfView;
+    private Account aModel;
+    DbManipulate dbm = new DbManipulate();
+    DbConnection dbc = new DbConnection();
 
-public class MainController {
-
-    private JPanel Contents;
-
-    private JTextField txtPassword;
-
-    private JComboBox comboCategory;
-    private JComboBox comboAccountName;
-    private JComboBox comboSpecific;
-    private JButton btnPasswordCopy;
-    private JButton btnLoginIdCopy;
-    private JButton addButton;
-    private JLabel loginIDLabel;
-    private JLabel passwordLabel;
-    private JLabel accountNameLabel;
-    private JLabel categoryLabel;
+    public MainController(Account aModel, MainForm mfView) {
+        this.mfView = mfView;
+        this.aModel = aModel;
+    }
 
     public MainController() {
-        PalletesController pc = new PalletesController();
-        addButton.setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
-        accountNameLabel.setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
-        categoryLabel.setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
-        loginIDLabel.setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
-        passwordLabel.setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
-
-        btnLoginIdCopy.setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
-        btnPasswordCopy.setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
-
-        comboCategory.setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
-        comboAccountName.setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
-        comboSpecific.setFont(new Font(pc.getFont(),Font.PLAIN,pc.getSize()));
-
-        addButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                AddAccountController aac = new AddAccountController();
-                aac.Launch();
-            }
-        });
 
     }
 
-    public static void main(String[] args) {
-        UIChange();
+    @Override
+    public List<String> getCategory() {
+        try {
+            dbm.setPs(dbc.dbConnection().prepareStatement(dbm.getCATEGORY_QUERY()));
+            dbm.setRs(dbm.getPs().executeQuery());
+            aModel.getCategory().add("hello");
+            //System.out.println(aModel.getCategory());
+//            while (dbm.getRs().next()) {
+//                //aModel.getCategory().add("1");
+//                aModel.getCategory().add("hello");
+//                //aModel.getCategory().add(dbm.getRs().getString("title"));
+//                //System.out.println(aModel.getCategory());
+//                //aModel.getCategory().add(dbm.getRs().getString("title"));
+//                System.out.println(dbm.getRs().getString("title"));
+//            }
 
-        JFrame mainFrame = new JFrame("Main");
-        mainFrame.setContentPane((new MainController().Contents));
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.pack();
-        mainFrame.setVisible(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+        return aModel.getCategory();
     }
+
 }
